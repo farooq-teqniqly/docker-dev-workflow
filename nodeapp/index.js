d9f058dba0b163a84d5ee76a589842be298696b9
Farooq Mahmud <farooq@teqniqly.com>
2020-11-04 13:35:21 -0800

    Containerized node app

---
+++ nodeapp/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Application listening at 0.0.0.0:3000");
+});
+
+app.get("/", (req, res) => {
+  res.send("Sample Application: Hello World!");
+});
+
+const hobbies = ["Swimming", "Diving", "Jogging", "Cooking", "Singing"];
+
+app.get("/hobbies", (req, res) => {
+  res.send(hobbies);
+});
